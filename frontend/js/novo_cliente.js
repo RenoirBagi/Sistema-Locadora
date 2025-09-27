88e9f0007087f8737438f06dc5afe1d4e124a801
omanothur <arthuralc2017@gmail.com>
2025-09-27 19:22:50 -0300

    Adicionando a tela para cadastrar clientes

---
+++ frontend/js/novo_cliente.js
@@ -0,0 +1,28 @@
+const API_URL = "http://localhost:5000";
+
+document.getElementById("form-cliente").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const data = {
+    "cpf": e.target.cpf_cliente.value,
+    "nome": e.target.nome_cliente.value,
+    "idade": e.target.idade_cliente.value,
+    "contato": e.target.contato_cliente.value,
+    "endereco": e.target.endereco_cliente.value
+  };
+
+  console.log(data);
+
+  const response = await fetch(`${API_URL}/clientes/`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  });
+
+  const result = await response.json();
+  alert(result.mensagem || result.erro);
+
+  if (response.ok) {
+    window.location.href = "index.html";
+  }
+});
