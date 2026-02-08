0d79d285891d1f0e2a5d0cb25afe337a96cf7bc4
RenoirBagi <renoirbagi@gmail.com>
2026-02-08 17:47:40 -0300

    Funcionalidade: Criar Filme

---
+++ frontend/js/novo_filme.js
@@ -0,0 +1,27 @@
+const API_URL = "http://localhost:5000";
+
+document.getElementById("form-filme").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const data = {
+    "titulo": e.target.titulo.value,
+    "genero": e.target.genero.value,
+    "ano": e.target.ano.value,
+    "preco": e.target.preco.value,
+  };
+
+  console.log(data);
+
+  const response = await fetch(`${API_URL}/filmes/`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  });
+
+  const result = await response.json();
+  alert(result.mensagem || result.erro);
+
+  if (response.ok) {
+    window.location.href = "index.html";
+  }
+});
